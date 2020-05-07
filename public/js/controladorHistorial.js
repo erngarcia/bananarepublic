679781f4c4a7cb49b11c74e0970dddaca68d1923
ernest-garciaestrada-cenfotec <lgarciae@ucenfotec.ac.cr>
2020-05-07 16:11:28 -0600

    funciona el filtro.
    hay un bug cuando se ponen numeros entre 20-60 en el start token. voy a investigar

--- public/js/controladorHistorial.js
+++ public/js/controladorHistorial.js
@@ -1,22 +1,47 @@
 'use strict';
 const tbody = document.querySelector('#tbl-metricas tbody');
+const inputFiltro = document.querySelector('#txtfiltro');
+
+
 
 let mostrar_metricas = async() => {
-    let metricas = await listar_metricas();
+	let metricas = await listar_metricas();
+	let filtro = inputFiltro.value;
+
     tbody.innerHTML = '';
 
     for (let i = 0; i < metricas.length; i++) {
-        let fila = tbody.insertRow();
-        fila.insertCell().innerHTML = metricas[i]['login'];
-        fila.insertCell().innerHTML = metricas[i]['cycle_name'];
-        fila.insertCell().innerHTML = moment(metricas[i]['fecha']).format('DD-MM-YYYY');
-        fila.insertCell().innerHTML = metricas[i]['start_time'];
-        fila.insertCell().innerHTML = metricas[i]['end_time'];
-        fila.insertCell().innerHTML = metricas[i]['start_token'];
-        fila.insertCell().innerHTML = metricas[i]['end_token'];
-        fila.insertCell().innerHTML = (metricas[i]['end_token']-metricas[i]['start_token'])/((metricas[i]['end_time']-metricas[i]['start_time'])/60);
-        fila.insertCell().innerHTML = (metricas[i]['end_token']-metricas[i]['start_token']);
+    	if(metricas[i]['login'].includes(filtro)){
+	    	let fila = tbody.insertRow();
+	        fila.insertCell().innerHTML = metricas[i]['login'];
+	        fila.insertCell().innerHTML = metricas[i]['cycle_name'];
+	        fila.insertCell().innerHTML = moment(metricas[i]['fecha']).format('DD-MM-YYYY');
+	        fila.insertCell().innerHTML = metricas[i]['start_time'];
+	        fila.insertCell().innerHTML = metricas[i]['end_time'];
+	        fila.insertCell().innerHTML = metricas[i]['start_token'];
+	        fila.insertCell().innerHTML = metricas[i]['end_token'];
+	        fila.insertCell().innerHTML = (metricas[i]['end_token']-metricas[i]['start_token'])/((metricas[i]['end_time']-metricas[i]['start_time'])/60);
+	        fila.insertCell().innerHTML = (metricas[i]['end_token']-metricas[i]['start_token']);
+    	}
+    	else if(metricas[i]['fecha'].includes(filtro)){
+	    	let fila = tbody.insertRow();
+	        fila.insertCell().innerHTML = metricas[i]['login'];
+	        fila.insertCell().innerHTML = metricas[i]['cycle_name'];
+	        fila.insertCell().innerHTML = moment(metricas[i]['fecha']).format('DD-MM-YYYY');
+	        fila.insertCell().innerHTML = metricas[i]['start_time'];
+	        fila.insertCell().innerHTML = metricas[i]['end_time'];
+	        fila.insertCell().innerHTML = metricas[i]['start_token'];
+	        fila.insertCell().innerHTML = metricas[i]['end_token'];
+	        fila.insertCell().innerHTML = (metricas[i]['end_token']-metricas[i]['start_token'])/((metricas[i]['end_time']-metricas[i]['start_time'])/60);
+	        fila.insertCell().innerHTML = (metricas[i]['end_token']-metricas[i]['start_token']);
+    	}
+        
     }
 };
 
 mostrar_metricas();
+
+inputFiltro.addEventListener('keyup',mostrar_metricas);
+
+
+
