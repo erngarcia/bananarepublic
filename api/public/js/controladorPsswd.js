83739d6b9752532af514dc0cf68f73afc6d51e32
ernest-garciaestrada-cenfotec <lgarciae@ucenfotec.ac.cr>
2020-05-18 06:40:14 -0600

    pswd  added

---
+++ api/public/js/controladorPsswd.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let login = document.querySelector('#login');
+let pswd = document.querySelector('#password');
+const boton = document.querySelector('#boton');
+
+let login_form = () => {
+	if (login.value == "linguistic services" && pswd.value == "adscentral"){
+		window.open("index.html")
+	}else{
+		alert("Wrong user name or password")
+	}
+};
+
+boton.addEventListener('click', login_form);
