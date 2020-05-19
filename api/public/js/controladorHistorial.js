1101e94326dc08769f1b63dd2d1bf6641c0e1795
ernest-garciaestrada-cenfotec <lgarciae@ucenfotec.ac.cr>
2020-05-19 10:58:32 -0600

    new order for historial

--- api/public/js/controladorHistorial.js
+++ api/public/js/controladorHistorial.js
@@ -17,10 +17,9 @@ let mostrar_metricas = async() => {
 	        fila.insertCell().innerHTML = metricas[i]['cycle_name'];
 	       	fila.insertCell().innerHTML = metricas[i]['worktype'];
 	        fila.insertCell().innerHTML = moment(metricas[i]['fecha']).format('DD-MM-YYYY');
-	        fila.insertCell().innerHTML = metricas[i]['start_time'];
-	        fila.insertCell().innerHTML = metricas[i]['end_time'];
-	        fila.insertCell().innerHTML = metricas[i]['start_token'];
-	        fila.insertCell().innerHTML = metricas[i]['end_token'];
+	        fila.insertCell().innerHTML = metricas[i]['end_time']-metricas[i]['start_time'];
+	        // fila.insertCell().innerHTML = metricas[i]['start_token'];
+	        // fila.insertCell().innerHTML = metricas[i]['end_token'];
 	        fila.insertCell().innerHTML = ((parseInt(metricas[i]['end_token'])-parseInt(metricas[i]['start_token']))/((parseInt(metricas[i]['end_time'])-parseInt(metricas[i]['start_time']))/60)).toFixed(1);
 	        fila.insertCell().innerHTML = parseInt(metricas[i]['end_token'])-parseInt(metricas[i]['start_token']);
 
@@ -45,10 +44,11 @@ let mostrar_metricas = async() => {
 	        fila.insertCell().innerHTML = metricas[i]['cycle_name'];
 	       	fila.insertCell().innerHTML = metricas[i]['worktype'];
 	        fila.insertCell().innerHTML = moment(metricas[i]['fecha']).format('DD-MM-YYYY');
-	        fila.insertCell().innerHTML = metricas[i]['start_time'];
-	        fila.insertCell().innerHTML = metricas[i]['end_time'];
-	        fila.insertCell().innerHTML = metricas[i]['start_token'];
-	        fila.insertCell().innerHTML = metricas[i]['end_token'];
+	        fila.insertCell().innerHTML = metricas[i]['end_time']-metricas[i]['start_time'];
+	        // fila.insertCell().innerHTML = metricas[i]['start_time'];
+	        // fila.insertCell().innerHTML = metricas[i]['end_time'];
+	        // fila.insertCell().innerHTML = metricas[i]['start_token'];
+	        // fila.insertCell().innerHTML = metricas[i]['end_token'];
 	        fila.insertCell().innerHTML = ((parseInt(metricas[i]['end_token'])-parseInt(metricas[i]['start_token']))/((parseInt(metricas[i]['end_time'])-parseInt(metricas[i]['start_time']))/60)).toFixed(1);
 	        fila.insertCell().innerHTML = parseInt(metricas[i]['end_token'])-parseInt(metricas[i]['start_token']);
     	}
